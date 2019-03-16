af8f22ae72d3197d954fd99d4cf374af947ea818
bhuvaneshvar <bhuvaneshvar6@outlook.com>
2019-03-17 00:03:30 +0530

    Initial

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function images() {
+    var textCredits = {
+        'Photo by Fernando Hernandez on Unsplash',
+        'Photo by Fernando Hernandez on Unsplash',
+        '',
+        ''
+    };
+}
