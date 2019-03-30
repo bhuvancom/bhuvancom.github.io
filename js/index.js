6cf69b15d42ed6c57eb68f843b31c6028acfbb96
bhuvaneshvar <bhuvaneshvar6@outlook.com>
2019-03-30 14:52:38 +0530

    New theme

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function images() {
-    var textCredits = {
-        'Photo by Fernando Hernandez on Unsplash',
-        'Photo by Fernando Hernandez on Unsplash',
-        '',
-        ''
-    };
-}
